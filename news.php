ec94bb37fb9ba34182668a9a640d9dff368699f1
Joshua Lessard <jlessard93@gmail.com>
2018-09-10 22:41:42 -0400

    fixing faq, updated footer, cleaned up code and added new newsletter

--- news.php
+++ news.php
@@ -1,49 +1,42 @@
 <?php include 'sidebar.php'; ?>
 
-<a name="top"></a><br>
+	<a name="top"></a><br>
 
-<table width=80% align="center" cellpadding=30px>
-<tr>
-<td align="left">
-
-<h2 class="top" style="color:rgb(146, 65, 80);"><em>PTFU NEWS</em></h2><br>
-    
-<h3 class="header" style="color:rgb(146, 65, 80); padding-bottom:10px;">NEWSLETTERS</h3>
-
-    <a href="newsletters/e_news_spring_2015.pdf" target="_blank">Spring 2015</a><br><br>
-    <a href="newsletters/e_news_fall_2015.pdf" target="_blank">Fall 2015</a><br><br>
-    <a href="newsletters/e_news_april_2014.pdf" target="_blank">April 2014</a><br><br>
-    <a href="newsletters/e_news_oct_2014.pdf" target="_blank">October 2014</a><br><br>
-    <a href="newsletters/e_news_sep_2013.pdf" target="_blank">September 2013</a><br><br>
-    <a href="newsletters/e_news_feb_2013.pdf" target="_blank">February 2013</a><br><br>
-    <a href="newsletters/e_news_dec_2012.pdf" target="_blank">December 2012</a><br><br>
-    <a href="newsletters/e_news_sep_2012.pdf" target="_blank">September 2012</a><br><br>
-    <a href="newsletters/e_news_may_2012.pdf" target="_blank">May 2012</a><br><br>
-    <a href="newsletters/e_news_july_2011.pdf" target="_blank">July 2011</a> <br><br>
-      <a href="newsletters/e_news_aug_2010.pdf" target="_blank">August 2010</a><br><br>
-      <a href="newsletters/e_news_nov_2009.pdf" target="_blank">November 2009</a> <br><br>
-      <a href="newsletters/e_news_july_2009.pdf" target="_blank">July 2009</a> <br><br>
-      <a href="files/fact_sheet.pdf" target="_blank">January 2009</a><br><br>
-      
-      
-    
-<h3 class="header" style="color:rgb(146, 65, 80); padding-bottom:10px;">OTHER NEWS</h3>
-    
-    <a href="othernews/arbitration_decision.pdf" target="_blank">Arbitration Decision - October 2014</a><br><br>
-    <a href="othernews/true_story_of_academia_2014_Salon.pdf" target="_blank">The shocking true story of academia in 2014</a><br><br>
-    <a href="http://rhodycigar.com/2014/10/16/adjunct-professors-feel-devalued-by-pay/" target="_blank">The Good 5 Cent Cigar - Adjunct Professors Feel Devalued By Pay</a><br><br>
-    <a href="othernews/highlights.pdf" target="_blank">Highlights - April 2014</a><br><br>
-    <a href="othernews/pro_rata_salary_compensation_2014.pdf" target="_blank">Pro-Rata Salary Compensation - February 2014</a><br><br>
-    <a href="othernews/canidate_statements_2012_2014.pdf" target="_blank">Executive Committee Candidate Statements - 2012-14</a><br><br>
-    <a href="othernews/contract_negotiations_2012.pdf" target="_blank">Contract Negotiations - 2012</a><br><br>
-    <a href="othernews/ptfu_negotiation_statement_2012.pdf" target="_blank">Negotiation Statement - 2012</a><br><br>
-    <a href="othernews/collective_bargaining_gains_aug_2011.pdf" target="_blank">Collective Bargaining Gains - August 2011</a><br><br>
-    <a href="othernews/editorial.pdf" target="_blank">Editorial - October 2008</a><br><br>
-    <a href="othernews/cigar_oct_2008.pdf" target="_blank">Letter to the Editor - October 2008</a><br><br>
-    <br>
-    <!--<center><a href="index.html">Home</a> | <a href="#top">Top</a></center>-->
- 
-</td>
-</tr>
-</table>
+	<table width=80% align="center" cellpadding=30px>
+		<tr>
+			<td align="left">
+				<h2 class="top" style="color:rgb(146, 65, 80);"><em>PTFU NEWS</em></h2><br>
+				<h3 class="header" style="color:rgb(146, 65, 80); padding-bottom:10px;">NEWSLETTERS</h3>
+				<a href="newsletters/e_news_fall_2016.pdf" target="_blank">Fall 2016</a><br><br>
+				<a href="newsletters/e_news_spring_2015.pdf" target="_blank">Spring 2015</a><br><br>
+				<a href="newsletters/e_news_fall_2015.pdf" target="_blank">Fall 2015</a><br><br>
+				<a href="newsletters/e_news_april_2014.pdf" target="_blank">April 2014</a><br><br>
+				<a href="newsletters/e_news_oct_2014.pdf" target="_blank">October 2014</a><br><br>
+				<a href="newsletters/e_news_sep_2013.pdf" target="_blank">September 2013</a><br><br>
+				<a href="newsletters/e_news_feb_2013.pdf" target="_blank">February 2013</a><br><br>
+				<a href="newsletters/e_news_dec_2012.pdf" target="_blank">December 2012</a><br><br>
+				<a href="newsletters/e_news_sep_2012.pdf" target="_blank">September 2012</a><br><br>
+				<a href="newsletters/e_news_may_2012.pdf" target="_blank">May 2012</a><br><br>
+				<a href="newsletters/e_news_july_2011.pdf" target="_blank">July 2011</a> <br><br>
+				<a href="newsletters/e_news_aug_2010.pdf" target="_blank">August 2010</a><br><br>
+				<a href="newsletters/e_news_nov_2009.pdf" target="_blank">November 2009</a> <br><br>
+				<a href="newsletters/e_news_july_2009.pdf" target="_blank">July 2009</a> <br><br>
+				<a href="files/fact_sheet.pdf" target="_blank">January 2009</a><br><br>
+				<h3 class="header" style="color:rgb(146, 65, 80); padding-bottom:10px;">OTHER NEWS</h3>
+				<a href="othernews/arbitration_decision.pdf" target="_blank">Arbitration Decision - October 2014</a><br><br>
+				<a href="othernews/true_story_of_academia_2014_Salon.pdf" target="_blank">The shocking true story of academia in 2014</a><br><br>
+				<a href="http://rhodycigar.com/2014/10/16/adjunct-professors-feel-devalued-by-pay/" target="_blank">The Good 5 Cent Cigar - Adjunct Professors Feel Devalued By Pay</a><br><br>
+				<a href="othernews/highlights.pdf" target="_blank">Highlights - April 2014</a><br><br>
+				<a href="othernews/pro_rata_salary_compensation_2014.pdf" target="_blank">Pro-Rata Salary Compensation - February 2014</a><br><br>
+				<a href="othernews/canidate_statements_2012_2014.pdf" target="_blank">Executive Committee Candidate Statements - 2012-14</a><br><br>
+				<a href="othernews/contract_negotiations_2012.pdf" target="_blank">Contract Negotiations - 2012</a><br><br>
+				<a href="othernews/ptfu_negotiation_statement_2012.pdf" target="_blank">Negotiation Statement - 2012</a><br><br>
+				<a href="othernews/collective_bargaining_gains_aug_2011.pdf" target="_blank">Collective Bargaining Gains - August 2011</a><br><br>
+				<a href="othernews/editorial.pdf" target="_blank">Editorial - October 2008</a><br><br>
+				<a href="othernews/cigar_oct_2008.pdf" target="_blank">Letter to the Editor - October 2008</a><br><br>
+				<br>
+				<!--<center><a href="index.html">Home</a> | <a href="#top">Top</a></center>-->
+			</td>
+		</tr>
+	</table>
 <?php include 'footer.php'; ?>
