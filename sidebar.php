813475db631e32a5e8df773ba7e72bbf2de7ef17
Joshua Lessard <jlessard93@gmail.com>
2019-08-12 19:25:26 -0400

    adding google analytics tracking code to the page
    Signed-off-by: Joshua Lessard <jlessard93@gmail.com>

--- sidebar.php
+++ sidebar.php
@@ -1,69 +1,83 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>URI PTFU</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145552503-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-145552503-1');
+    </script>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>URI PTFU</title>
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/simple-sidebar.css" rel="stylesheet">
     <link rel="stylesheet" href="css/font-awesome.css">
-    <link rel="shortcut icon" href="images/ptfulogo_rev(fav).ico" >
+    <link rel="shortcut icon" href="images/ptfulogo_rev(fav).ico">
 </head>
 
 <body>
-	<div id="wrapper">
+<div id="wrapper">
 
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
             <img src="images/ptfulogo_rev.jpg" alt="" width="200" height="200" hspace="25" vspace="10">
-                <li class="sidebar-brand" style="padding-left:45px;">
-                	<a href="index.php"><strong>Home</strong></a>
-                </li>
-                <li>
-                    <a href="files/ptfu_welcome_letter.pdf" target="_blank">Welcome!</a>
-                </li>
-                <li>
-                    <a href="files/CONSTITUTION.BY-LAWS_AMENDED.DECEMBER.2017.pdf" target="_blank">Constitution &amp; By-Laws</a>
-                </li>
-                <li>
-                    <a href="files/URI_PTFU_contract_2017-2020.pdf" target="_blank">Contract 2017-2020</a>
-                </li>
-                <li>
-                    <a href="faq.php">FAQ</a>
-                </li>
-                <li>
-                    <a href="files/fact_sheet.pdf" target="_blank">URI PTFU Fact Sheet</a>
-                </li>
-	            <li>
-		            <a href="files/Contract_gains_2017_2020.pdf" target="_blank">PTFU Contract Gains 2017-2020</a>
-	            </li>
-                <li>
-                    <a href="news.php">News</a>
-                </li>
-                <li class="sidebar-brand">
-                    <a href="#"><strong>Forms and Links</strong></a>
-                </li>
-                <li>
-                	<a href="files/salary_advancement_form_with_info.pdf" target="_blank">Level Advancement Form</a>
-                </li>
-                <li>
-                	<a href="files/course_request_availability_form.pdf" target="_blank"> Availability Form</a>
-                </li>
-                <li>
-                	<a href="files/uri_ptfu_grievance_form.pdf" target="_blank">Grievance Form</a>
-                </li>
-                <li>
-                	<a href="files/grievance_procedure_time_frame_2014.pdf" target="_blank">Grievance Procedure Time Frame</a>
-                </li>
-                <li>
-                	<a href="files/membership_authorization_card.pdf" target="_blank">Membership Application</a>
-                </li>
-                <li>
-                	<a href="http://www.aaup.org/" target="_blank">AAUP National</a>
-                </li>
-                <li>
-                	<a href="http://www.uri.edu/" target="_blank" style="margin-bottom:50%;">University of Rhode Island</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /#sidebar-wrapper -->
+            <li class="sidebar-brand" style="padding-left:45px;">
+                <a href="index.php"><strong>Home</strong></a>
+            </li>
+            <li>
+                <a href="files/ptfu_welcome_letter.pdf" target="_blank">Welcome!</a>
+            </li>
+            <li>
+                <a href="files/CONSTITUTION.BY-LAWS_AMENDED.DECEMBER.2017.pdf" target="_blank">Constitution &amp; By-Laws</a>
+            </li>
+            <li>
+                <a href="files/URI_PTFU_contract_2017-2020.pdf" target="_blank">Contract 2017-2020</a>
+            </li>
+            <li>
+                <a href="faq.php">FAQ</a>
+            </li>
+            <li>
+                <a href="files/fact_sheet.pdf" target="_blank">URI PTFU Fact Sheet</a>
+            </li>
+            <li>
+                <a href="files/Contract_gains_2017_2020.pdf" target="_blank">PTFU Contract Gains 2017-2020</a>
+            </li>
+            <li>
+                <a href="news.php">News</a>
+            </li>
+            <li class="sidebar-brand">
+                <a href="#"><strong>Forms and Links</strong></a>
+            </li>
+            <li>
+                <a href="files/salary_advancement_form_with_info.pdf" target="_blank">Level Advancement Form</a>
+            </li>
+            <li>
+                <a href="files/course_request_availability_form.pdf" target="_blank"> Availability Form</a>
+            </li>
+            <li>
+                <a href="files/uri_ptfu_grievance_form.pdf" target="_blank">Grievance Form</a>
+            </li>
+            <li>
+                <a href="files/grievance_procedure_time_frame_2014.pdf" target="_blank">Grievance Procedure Time Frame</a>
+            </li>
+            <li>
+                <a href="files/membership_authorization_card.pdf" target="_blank">Membership Application</a>
+            </li>
+            <li>
+                <a href="http://www.aaup.org/" target="_blank">AAUP National</a>
+            </li>
+            <li>
+                <a href="http://www.uri.edu/" target="_blank" style="margin-bottom:50%;">University of Rhode Island</a>
+            </li>
+        </ul>
+    </div>
+    <!-- /#sidebar-wrapper -->
